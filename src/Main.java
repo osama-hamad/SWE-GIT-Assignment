e6dbeffde94a8126193a3320e0cf2207bcf2b768
Mona Ahmed <34746537+MonaAhmed810@users.noreply.github.com>
2018-11-27 14:40:49 +0200

    add Min3Numbers Function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
+    public static void Min3Numbers() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter array size");
+        int sz = in.nextInt();
+        Integer arr[] = new Integer[sz];
+        for (int i = 0; i < sz; i++)
+            arr[i] = in.nextInt();
+        Collections.sort(Arrays.asList(arr));
+        System.out.println("Minimum 3 Numbers are :");
+        for (int i = 0; i < 3 && i < sz; i++)
+            System.out.println(arr[i]);
+    }
 
     public static void main(String[] args) {
-
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose Function Number:");
+        System.out.println("1. Get minimum 3 numbers");
+        int reply = in.nextInt();
+        switch (reply) {
+            case 1:
+                Min3Numbers();
+                break;
+        }
     }
 }
